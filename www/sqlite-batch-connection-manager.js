287ff779883bd9d9d4a435c85b04d4f94f1663d8
Christopher J. Brody <chris.brody+brodybits@gmail.com>
2020-05-22 01:30:10 -0400

    apply workaround for large results on Android
    with memory test now updated

--- www/sqlite-batch-connection-manager.js
+++ www/sqlite-batch-connection-manager.js
@@ -11,7 +11,42 @@ function openDatabaseConnection (options, cb, errorCallback) {
 }
 
 function executeBatch (connectionId, batchList, cb) {
-  cordova.exec(cb, null, 'SQLiteBatchConnectionManager', 'executeBatch', [
+  const batchLength = batchList.length
+
+  var results = []
+
+  var isPartial = false
+
+  var partialResult = null
+
+  var partialRows = null
+
+  function success (data) {
+    if (isPartial) {
+      partialRows.push(data)
+      if (partialRows.length === partialResult.rowsLength) {
+        results = results.concat({
+          status: 0,
+          columns: partialResult.columns,
+          rows: partialRows
+        })
+        isPartial = false
+      }
+    } else if (data.partial) {
+      isPartial = true
+      partialResult = data
+      partialRows = []
+    } else {
+      results = results.concat(data)
+    }
+
+    if (results.length === batchLength) {
+      cb(results)
+      results = []
+    }
+  }
+
+  cordova.exec(success, null, 'SQLiteBatchConnectionManager', 'executeBatch', [
     connectionId,
     // avoid potential behavior such as crashing in case of invalid
     // batchList due to possible API abuse
