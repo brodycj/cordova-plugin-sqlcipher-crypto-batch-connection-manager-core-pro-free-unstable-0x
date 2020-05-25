3eb828515ab889286425a303254e03b678833b79
Christopher J. Brody <chris.brody+brodybits@gmail.com>
2020-05-24 21:00:38 -0400

    update demo to use database files
    using file plugins
    
    as well as memory database demo
    
    update Makefile

--- demo/www/app.js
+++ demo/www/app.js
@@ -10,18 +10,116 @@ function log (text) {
   console.log(text)
 }
 
-function onReady () {
-  log('deviceready event received')
+const DATABASE_FILE_NAME = 'demo.db'
 
+// SQLITE_OPEN_READWRITE | SQLITE_OPEN_CREATE
+// ref: https://www.sqlite.org/c3ref/open.html
+const OPEN_DATABASE_FILE_FLAGS = 6
+
+function openMemoryDatabaseConnection (openCallback, errorCallback) {
   window.sqliteBatchConnectionManager.openDatabaseConnection(
     { fullName: ':memory:', flags: 2 },
-    openCallback
+    openCallback,
+    errorCallback
   )
 }
 
-function openCallback (connectionId) {
+function openFileDatabaseConnection (name, openCallback, errorCallback) {
+  window.sqliteStorageFile.resolveAbsolutePath(
+    {
+      name: name,
+      // TEMPORARY & DEPRECATED value, as needed for iOS & macOS ("osx"):
+      location: 2
+    },
+    function (path) {
+      log('database file path: ' + path)
+
+      window.sqliteBatchConnectionManager.openDatabaseConnection(
+        { fullName: path, flags: OPEN_DATABASE_FILE_FLAGS },
+        openCallback,
+        errorCallback
+      )
+    }
+  )
+}
+
+function openCacheFileDatabaseConnection (name, openCallback, errorCallback) {
+  window.resolveLocalFileSystemURL(
+    // portable across Android, iOS, & macOS ("osx"):
+    cordova.file.cacheDirectory,
+    function (entry) {
+      const dataDirectoryUrl = entry.toURL()
+
+      log('data directory url: ' + dataDirectoryUrl)
+
+      // hacky, working solution:
+      const path = dataDirectoryUrl.substring(7) + name
+
+      log('database cache file path: ' + path)
+
+      window.sqliteBatchConnectionManager.openDatabaseConnection(
+        { fullName: path, flags: OPEN_DATABASE_FILE_FLAGS },
+        openCallback,
+        errorCallback
+      )
+    }
+  )
+}
+
+function onReady () {
+  log('deviceready event received')
+  startMemoryDatabaseDemo()
+}
+
+function startMemoryDatabaseDemo () {
+  openMemoryDatabaseConnection(
+    function (id) {
+      log('memory database connection id: ' + id)
+
+      window.sqliteBatchConnectionManager.executeBatch(
+        id,
+        [['SELECT UPPER(?)', ['Text']]],
+        function (results) {
+          log(JSON.stringify(results))
+          startFileDatabaseDemo()
+        }
+      )
+    },
+    function (error) {
+      log('UNEXPECTED OPEN MEMORY DATABASE ERROR: ' + error)
+    }
+  )
+}
+
+function startFileDatabaseDemo () {
+  openFileDatabaseConnection(
+    DATABASE_FILE_NAME,
+    openDatabaseFileCallback,
+    function (e) {
+      log('UNEXPECTED OPEN ERROR: ' + e)
+    }
+  )
+}
+
+function openDatabaseFileCallback (connectionId) {
   log('open connection id: ' + connectionId)
 
+  // ERROR TEST - file name with incorrect flags:
+  window.sqliteBatchConnectionManager.openDatabaseConnection(
+    { fullName: 'dummy.db', flags: 0 },
+    function (_ignored) {
+      log('FAILURE - unexpected open success callback received')
+    },
+    function (e) {
+      log('OK - received error callback as expected for incorrect open call')
+
+      // CONTINUE with batch demo, with the correct connectionId:
+      batchDemo(connectionId)
+    }
+  )
+}
+
+function batchDemo (connectionId) {
   log('starting batch demo for connection id: ' + connectionId)
   window.sqliteBatchConnectionManager.executeBatch(
     connectionId,
@@ -51,4 +149,52 @@ function batchCallback (batchResults) {
   // show batch results in JSON string format (on all platforms)
   log('received batch results')
   log(JSON.stringify(batchResults))
+
+  startReaderDemo()
+}
+
+function startReaderDemo () {
+  openFileDatabaseConnection(
+    DATABASE_FILE_NAME,
+    function (id) {
+      log('read from another connection id: ' + id)
+
+      window.sqliteBatchConnectionManager.executeBatch(
+        id,
+        [['SELECT * FROM Testing', []]],
+        function (res) {
+          log(JSON.stringify(res))
+          startCacheFileDemo()
+        }
+      )
+    },
+    function (error) {
+      log('UNEXPECTED OPEN ERROR: ' + error)
+    }
+  )
+}
+
+function startCacheFileDemo () {
+  openCacheFileDatabaseConnection(
+    DATABASE_FILE_NAME,
+    function (id) {
+      log('cache file database connection id: ' + id)
+
+      window.sqliteBatchConnectionManager.executeBatch(
+        id,
+        [
+          ['DROP TABLE IF EXISTS Testing', []],
+          ['CREATE TABLE Testing (data NOT NULL)', []],
+          ["INSERT INTO Testing VALUES ('test data')", []],
+          ['SELECT * FROM Testing', []]
+        ],
+        function (results) {
+          log(JSON.stringify(results))
+        }
+      )
+    },
+    function (error) {
+      log('UNEXPECTED OPEN ERROR: ' + error)
+    }
+  )
 }
