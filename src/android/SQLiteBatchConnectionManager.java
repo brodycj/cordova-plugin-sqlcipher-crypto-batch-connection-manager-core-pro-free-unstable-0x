b42fb3f00b5523c36051a204de661a2a05aac9f9
Christopher J. Brody <chris.brody+brodybits@gmail.com>
2020-05-24 20:58:54 -0400

    introduce working plugin implementation
    that is supported by additional tasks in Makefile
    
    with separate, private executeBatchNow method on both Android & iOS
    
    that can be used to support background threading very easily
    
    requires use of some make targets to add & build
    some distribution dependencies
    
    add working memory database demo code to the demo app
    
    update README.md with extremely basic information
    
    from sqlite-batch-connection-core project:
    
    https://github.com/brodybits/sqlite-batch-connection-core-2020-01
    
    update .gitignore to ignore downloaded & generated artifacts
    
    apply extra cleanup: rename internal execute argument on Android

--- src/android/SQLiteBatchConnectionManager.java
+++ src/android/SQLiteBatchConnectionManager.java
@@ -2,19 +2,74 @@
 
 package io.sqlc;
 
-import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaPlugin;
 
 import org.json.JSONArray;
+import org.json.JSONObject;
 
 public class SQLiteBatchConnectionManager extends CordovaPlugin {
   @Override
-  public boolean execute(String action, JSONArray args, CallbackContext cbc) {
-    switch (action) {
+  public boolean execute(String method, JSONArray data, CallbackContext cbc) {
+    switch(method) {
       case "openDatabaseConnection":
-        cbc.success(args);
-      return true;
+        openDatabaseConnection(data, cbc);
+        break;
+      case "executeBatch":
+        executeBatch(data, cbc);
+        break;
+      default:
+        return false;
     }
-    return false;
+    return true;
+  }
+
+  static private void
+  openDatabaseConnection(JSONArray args, CallbackContext cbc) {
+    try {
+      final JSONObject options = args.getJSONObject(0);
+
+      final String fullName = options.getString("fullName");
+
+      final int flags = options.getInt("flags");
+
+      final int mydbc = SQLiteBatchCore.openBatchConnection(fullName, flags);
+
+      if (mydbc < 0) {
+        cbc.error("open error: " + -mydbc);
+      } else {
+        cbc.success(mydbc);
+      }
+    } catch(Exception e) {
+      // NOT EXPECTED - internal error:
+      cbc.error(e.toString());
+    }
+  }
+
+  static private void executeBatch(JSONArray args, CallbackContext cbc) {
+    try {
+      final int mydbc = args.getInt(0);
+
+      JSONArray data = args.getJSONArray(1);
+
+      // Background threading is under future consideration at this point
+      // (expected to be straightforward for both Android & iOS)
+      executeBatchNow(mydbc, data, cbc);
+    } catch(Exception e) {
+      // NOT EXPECTED - internal error:
+      cbc.error(e.toString());
+    }
+  }
+
+  static private void
+  executeBatchNow(final int mydbc, JSONArray data, CallbackContext cbc) {
+    try {
+      JSONArray results = SQLiteBatchCore.executeBatch(mydbc, data);
+
+      cbc.success(results);
+    } catch(Exception e) {
+      // NOT EXPECTED - internal error:
+      cbc.error(e.toString());
+    }
   }
 }
